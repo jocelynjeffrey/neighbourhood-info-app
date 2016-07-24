7ef609177d818ae63e90567ae9b5c7697dd59234
Jocelyn Jeffrey <jocelyn@Jocelyns-MacBook-Air.local>
2016-07-24 15:39:38 -0400

    markers added to map

--- js/map.js
+++ js/map.js
@@ -1,14 +1,38 @@
 var map;
-var myLatLong = { lat: 43.684820, lng: -79.397876 };
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('googleMap'), {
-    center: myLatLong,
-    zoom: 16
+    center: { lat: 43.684820, lng: -79.397876 },
+    zoom: 15
   });
 
-  var marker = new google.maps.Marker({
-  	position: myLatLong,
-  	map: map, 
-  	title: 'house!'
+  var home = new google.maps.Marker({
+    position: { lat: 43.684820, lng: -79.397876 },
+    map: map,
+    title: 'house!'
+  });
+
+  var dogPark = new google.maps.Marker({
+    position: {lat: 43.6828344, lng: -79.4087779},
+    map: map,
+    title: 'dogpark!'
+  });
+
+  var rosedaleRavine = new google.maps.Marker({
+    position: {lat: 43.68838, lng: -79.38768},
+    map: map,
+    title: 'ravine!'
+  });
+
+    var rosedaleReservoir = new google.maps.Marker({
+    position: {lat: 43.685557, lng: -79.390325},
+    map: map,
+    title: 'reservoir!'
+  });
+
+    var summerhill = new google.maps.Marker({
+    position: {lat: 43.6803975, lng: -79.3909196},
+    map: map,
+    title: 'summerhill!'
   });
 }
