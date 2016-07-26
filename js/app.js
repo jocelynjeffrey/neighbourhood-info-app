d70722d1ed5886f64e84b1bf87fd4bfd35d0813e
Jocelyn Jeffrey <jocelyn@Jocelyns-MacBook-Air.local>
2016-07-25 21:11:37 -0400

    closure added

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-// This is a simple *viewmodel* - JavaScript that defines 
-//the data and behavior of your UI
-
 $(function(){
+
+var model = initMap();
 
 var AppViewModel = {
 	init: function(){
   	this.location = ko.observable("Toronto");
 		ko.applyBindings(AppViewModel);
 		view.init();
+
+		$.each(model, function(i, model){
+			return function(){
+				console.log(model.name)
+			}(model)
+		});
 	}
 
 };
 
 var view = {
   init: function() {
-  	console.log('view initialized');
+  	// console.log('view initialized');
 
   },
 
