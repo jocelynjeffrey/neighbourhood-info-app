8d8a35adb6d4e0b381a3ba29b99de9782b3e3b6c
Jocelyn Jeffrey <jocelyn.jeffrey@ca.wunderman.com>
2016-08-02 17:03:34 -0400

    filter workinggit add -A

--- js/app.js
+++ js/app.js
@@ -2,19 +2,44 @@ var mapLocations = locations;
 
 var Location = function(locationObj){
 	this.name = ko.observable(locationObj.name);
+  this.title = ko.observable(locationObj.title);
 };
 
 var AppViewModel = function(){
 	var self = this;
 
+  self.query = ko.observable('');
+
 	self.locationsList = ko.observableArray();
+  console.log(self.locationsList);
 
 	mapLocations.forEach(function(locationItem){
 		self.locationsList.push( new Location (locationItem));
-	})
+    console.log(self.locationsList)
+	});
 
-	self.currentLocation = ko.observableArray( self.locationsList()) ;
+	// self.currentLocation = ko.observableArray( self.locationsList()) ;
 
+
+
+
+  self.search = function(value) {
+
+    console.log(value)
+    // remove all the current locations, which removes them from the view
+   self.locationsList.removeAll();
+
+    for(var x in mapLocations) {
+              
+      if(mapLocations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        self.locationsList.push(mapLocations[x]);
+      }
+
+      
+    }
+  }
+
+  self.query.subscribe(self.search);
 };
 
 ko.applyBindings(new AppViewModel());
--- js/map.js
+++ js/map.js
@@ -12,15 +12,6 @@ function initMap() {
 
 
 function setMarkers(map) {
- // for (var i = 0; i < locations.length; i++) {
- //    var location = locations[i];
- //    var marker = new google.maps.Marker({
- //      position: {lat: location[1], lng: location[2]},
- //      map: map,
- //      title: location[0]
- //    });
- //  }
-
 
  locations.forEach(function(location){
       var marker = new google.maps.Marker({
@@ -31,15 +22,6 @@ function setMarkers(map) {
     });
  })
 }
-
-
-// var locations = [
-//   ['Home', 43.684820, -79.397876],
-//   ['Deer Park Dog Park', 43.6828344, -79.4087779],
-//   ['Rosedale Ravine', 43.68838, -79.4087779],
-//   ['Rosedale Reservoir', 43.685557, -79.390325],
-//   ['Summerhill', 43.6803975, -79.3909196]
-// ];
 
 var locations = [
     {
