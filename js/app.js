bcb08fa55a488b76415c1f47462bae69f78fdec3
Jocelyn Jeffrey <jocelyn@Jocelyns-MacBook-Air.local>
2016-08-06 16:38:11 -0400

    all component code, require.js stripped out

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 var mapLocations = googleMap.locations();
 
 var Location = function(locationObj){
@@ -7,22 +5,9 @@ var Location = function(locationObj){
   this.title = ko.observable(locationObj.title);
 };
 
-function Product(name, rating) {
-    this.name = name;
-    this.userRating = ko.observable(rating || null);
-}
-
 var AppViewModel = function(){
   var self = this;
-
-      self.products = [
-        new Product('Garlic bread'),
-        new Product('Pain au chocolat'),
-        new Product('Seagull spaghetti', 'like') // This one was already 'liked'
-    ];
-
   self.query = ko.observable('');
-
   self.locationsList = ko.observableArray();
 
   mapLocations.forEach(function(locationItem){
@@ -42,27 +27,6 @@ var AppViewModel = function(){
   }
 
   self.query.subscribe(self.search);
-
-var elmInstance = document.getElementById('test');  
-
-ko.components.register('like-widget', {
-    viewModel: function(params) {
-        // Data: value is either null, 'like', or 'dislike'
-        this.chosenValue = params.value;
-         
-        // Behaviors
-        this.like = function() { this.chosenValue('like'); }.bind(this);
-        this.dislike = function() { this.chosenValue('dislike'); }.bind(this);
-    },
-    template:
-        '<div class="like-or-dislike" data-bind="visible: !chosenValue()">\
-            <button data-bind="click: like">Like it</button>\
-            <button data-bind="click: dislike">Dislike it</button>\
-        </div>\
-        <div class="result" data-bind="visible: chosenValue">\
-            You <strong data-bind="text: chosenValue"></strong> it\
-        </div>'
-});
 
 };
 
