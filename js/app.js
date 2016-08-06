b644897cf7bca2814019466025feb639e1303282
Jocelyn Jeffrey <jocelyn@Jocelyns-MacBook-Air.local>
2016-08-06 10:58:59 -0400

    test component added and working

--- js/app.js
+++ js/app.js
@@ -1,20 +1,50 @@
+ko.components.register('like-widget', {
+    viewModel: function(params) {
+        // Data: value is either null, 'like', or 'dislike'
+        this.chosenValue = params.value;
+         
+        // Behaviors
+        this.like = function() { this.chosenValue('like'); }.bind(this);
+        this.dislike = function() { this.chosenValue('dislike'); }.bind(this);
+    },
+    template:
+        '<div class="like-or-dislike" data-bind="visible: !chosenValue()">\
+            <button data-bind="click: like">Like it</button>\
+            <button data-bind="click: dislike">Dislike it</button>\
+        </div>\
+        <div class="result" data-bind="visible: chosenValue">\
+            You <strong data-bind="text: chosenValue"></strong> it\
+        </div>'
+});
+
 var mapLocations = locations;
 
 var Location = function(locationObj){
-	this.name = ko.observable(locationObj.name);
+  this.name = ko.observable(locationObj.name);
   this.title = ko.observable(locationObj.title);
 };
 
+function Product(name, rating) {
+    this.name = name;
+    this.userRating = ko.observable(rating || null);
+}
+
 var AppViewModel = function(){
-	var self = this;
+  var self = this;
+
+      self.products = [
+        new Product('Garlic bread'),
+        new Product('Pain au chocolat'),
+        new Product('Seagull spaghetti', 'like') // This one was already 'liked'
+    ];
 
   self.query = ko.observable('');
 
-	self.locationsList = ko.observableArray();
+  self.locationsList = ko.observableArray();
 
-	mapLocations.forEach(function(locationItem){
-		self.locationsList.push( new Location (locationItem));
-	});
+  mapLocations.forEach(function(locationItem){
+    self.locationsList.push( new Location (locationItem));
+  });
 
 
   self.search = function(value) {
@@ -34,3 +64,4 @@ var AppViewModel = function(){
 };
 
 ko.applyBindings(new AppViewModel());
+
