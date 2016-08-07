d49faba5f851c7d902c36997a57bfab5baba821c
Jocelyn Jeffrey <jocelyn@Jocelyns-MacBook-Air.local>
2016-08-07 18:06:55 -0400

    wikipedia api wired, needs to be moved to proper file

--- js/app.js
+++ js/app.js
@@ -1,15 +1,55 @@
+// var nytHeader$ = $('#nytHeader');
+// var nytsnippet$ = $('#nytsnippet');
+
+// var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=ae725b912afe40a38533ade8b5a38592';
+// $.getJSON(nytimesURL, function (data){
+//   nytHeader$.text('NYT articles about ' + cityStr );
+//   nytsnippet$.text('test');
+
+//   articles = data.response.docs;
+//   articles.forEach(function(article){
+//     nytsnippet$.text(article.headline.main);
+//     // console.log(article.headline.main);
+//   });
+// }).fail(function(e){
+//   nytHeader$.text('NYT article could not be loaded');
+// });
+
+var cityStr = 'Toronto';
+var wikiElm$ = $('#wikiElm');
+var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='
+ + cityStr +
+ '&format=json&callback=wikiCallback';
+
+$.ajax({
+  url: wikiUrl,
+  dataType: "jsonp",
+  //by default, jsonp is set as callback when jsonp is set as data type
+  //jsonp: "callback",
+  success: function(response) {
+    var articleList = response[1];
+    for(var i = 0; i < articleList.length; i++){
+      articleStr = articleList[i];
+      var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+      wikiElm$.append('<li><a href="' + url + '">' + 
+        articleStr + '</a></li>');
+    };
+  }
+})
+
 
 var Location = function(locationObj){
   this.name = ko.observable(locationObj.name);
   this.title = ko.observable(locationObj.title);
-
+  this.imgSrc = ko.observable(locationObj.imgSrc);
+  this.imgAttribution = ko.observable(locationObj.imgAttribution);
   
   this.locationInfo = ko.computed(function() {
       
   }, this);
 
-
 };
+
 
 var AppViewModel = function(){
   var mapLocations = locations;
--- js/models/locations.js
+++ js/models/locations.js
@@ -4,27 +4,37 @@ var locations = (function() {
     {
       name: 'Casa Loma',
       position: { lat: 43.6780371, lng: -79.4116326 },
-      title: 'castle!'
+      title: 'castle!',
+      imgSrc: 'http://www.hlta.ca/wp-content/uploads/2015/05/CasaLoma.jpg',
+      imgAttribution: ''
     },
     {
       name: 'Deer Park',
       position: { lat: 43.6828344, lng: -79.4087779 },
-      title: 'park!'
+      title: 'park!',
+      imgSrc: 'http://www.torontoneighbourhoods.net/content/704/DSCF4584(1)_560x350.JPG',
+      imgAttribution: ''
     },
     {
       name: 'Royal Ontario Museum',
       position: { lat: 43.6677136, lng: -79.3969658 },
-      title: 'ROM!'
+      title: 'ROM!',
+      imgSrc: 'http://payload.cargocollective.com/1/5/166188/2251419/August%2026-%202008-portfolio-IMG-2_1140.jpg',
+      imgAttribution: 'Scott Norsworthy'
     },
     {
       name: 'Ramsden Park Trail',
       position: { lat: 43.6765595, lng: -79.3956885 },
-      title: 'reservoir!'
+      title: 'trail!',
+      imgSrc: 'http://static1.squarespace.com/static/5210f682e4b0a807b650c92b/5210f985e4b0a807b650d070/523f4a92e4b0496cd9c7b8d1/1380721035236/RamsdenPark3_July31-09.jpg',
+      imgAttribution: ''
     },
     {
       name: 'Spadina Museum',
       position: { lat: 43.6789949, lng: -79.4103276 },
-      title: 'spadina museum!'
+      title: 'spadina museum!',
+      imgSrc: 'http://s3.amazonaws.com/zweb-s3.uploads/visiontv/2013/04/04_Room_01_0033_OP.jpg',
+      imgAttribution: 'Maciek Lindwski'
     },
   ];
 
