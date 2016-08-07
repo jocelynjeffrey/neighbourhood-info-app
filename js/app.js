e411c3108030913a37b85432da570770afd47021
Jocelyn Jeffrey <jocelyn@Jocelyns-MacBook-Air.local>
2016-08-07 12:32:09 -0400

    location info available on click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-var mapLocations = locations;
 
 var Location = function(locationObj){
   this.name = ko.observable(locationObj.name);
@@ -6,19 +5,18 @@ var Location = function(locationObj){
 
   
   this.locationInfo = ko.computed(function() {
-      alert(this)
+      
   }, this);
 
 
 };
 
-
 var AppViewModel = function(){
+  var mapLocations = locations;
   var self = this;
-  self.query = ko.observable('');
-  
+
+  self.query = ko.observable(''); 
   self.locationsList = ko.observableArray([]);
-  // this.catList = ko.observableArray([]);
 
   mapLocations.forEach(function(locationItem){
     self.locationsList.push( new Location (locationItem));
@@ -26,14 +24,9 @@ var AppViewModel = function(){
 
   this.currentLocation = ko.observable( self.locationsList()[0]);
 
-  self.updateLocation = function(clickedLocation){
+  self.showLocationInfo = function(clickedLocation){
     self.currentLocation(clickedLocation);
   }
-
-  this.showLocationInfo = function(){
-    self.currentLocation().locationInfo();
-  };
-
 
   self.search = function(value) {
     // remove all the current locations, which removes them from the view
